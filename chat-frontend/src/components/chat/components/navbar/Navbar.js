3c5b34c41bcc7eb04558141441064e5d91504938
Young wook Jeon <ela87@naver.com>
2020-12-21 14:54:04 +0900

    Update user profile

--- chat-frontend/src/components/chat/components/navbar/Navbar.js
+++ chat-frontend/src/components/chat/components/navbar/Navbar.js
@@ -1,52 +1,125 @@
-import React, { useState, Fragment } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { logout } from '../../../../store/actions/auth';
-import Modal from '../modal/Modal';
-import './Navbar.scss';
+import React, { useState, Fragment } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { logout } from "../../../../store/actions/auth";
+import Modal from "../modal/Modal";
+import "./Navbar.scss";
 
 const Navbar = () => {
+  const user = useSelector((state) => state.authReducer.user);
+  const dispatch = useDispatch();
+  const [showProfileOptions, setShowProfileOptions] = useState(false);
+  const [showProfileModal, setShowProfileModal] = useState(false);
 
-    const user = useSelector(state => state.authReducer.user);
-    const dispatch = useDispatch();
-    const [showProfileOptions, setShowProfileOptions] = useState(false);
-    const [showProfileModal, setShowProfileModal] = useState(false);
+  const [firstName, setFirstName] = useState(user.firstName);
+  const [lastName, setLastName] = useState(user.lastName);
+  const [email, setEmail] = useState(user.email);
+  const [gender, setGender] = useState(user.gender);
+  const [password, setPassword] = useState("");
+  const [avatar, setAvatar] = useState("");
 
-    return (
-        <div id="navbar" className="card-shadow">
-            <h2>Chat.io</h2>
-            <div id="profile-menu" onClick={() => setShowProfileOptions(!showProfileOptions)} >
-                <img width="40" height="40" src={user.avatar} alt="Avatar" />
-                <p>{user.firstName} {user.lastName}</p>
-                <FontAwesomeIcon icon="caret-down" className="fa-icon" />
+  const submitForm = (e) => {
+    e.preventDefault();
+    const form = { firstName, lastName, email, gender, password, avatar };
+    const formData = new FormData();
 
-                {
-                    showProfileOptions && 
-                    <div id="profile-options">
-                        <p onClick={() => setShowProfileModal(true)}>Update profile</p>
-                        <p onClick={() => dispatch(logout())}>Logout</p>
-                    </div>
-                }
+    for (const key in form) {
+        formData.append(key, form[key]);
+    }
+    
+  };
 
-                {
-                    showProfileModal && 
-                    <Modal click={() => setShowProfileModal(false)}>
-                        <Fragment key="header">
-                            Modal Header
-                        </Fragment>
+  return (
+    <div id="navbar" className="card-shadow">
+      <h2>Chat.io</h2>
+      <div
+        id="profile-menu"
+        onClick={() => setShowProfileOptions(!showProfileOptions)}
+      >
+        <img width="40" height="40" src={user.avatar} alt="Avatar" />
+        <p>
+          {user.firstName} {user.lastName}
+        </p>
+        <FontAwesomeIcon icon="caret-down" className="fa-icon" />
 
-                        <Fragment key="body">
-                            Modal Body
-                        </Fragment>
+        {showProfileOptions && (
+          <div id="profile-options">
+            <p onClick={() => setShowProfileModal(true)}>Update profile</p>
+            <p onClick={() => dispatch(logout())}>Logout</p>
+          </div>
+        )}
 
-                        <Fragment key="footer">
-                            Modal Footer
-                        </Fragment>
-                    </Modal>
-                }
-            </div>
-        </div>
-    );
+        {showProfileModal && (
+          <Modal click={() => setShowProfileModal(false)}>
+            <Fragment key="header">
+              <h3 className="m-0">Update profile</h3>
+            </Fragment>
+
+            <Fragment key="body">
+              <form>
+                <div className="input-field mb-1">
+                  <input
+                    onChange={(e) => setFirstName(e.target.value)}
+                    value={firstName}
+                    required="required"
+                    type="text"
+                    placeholder="First name"
+                  />
+                </div>
+                <div className="input-field mb-1">
+                  <input
+                    placeholder="Last name"
+                    onChange={(e) => setLastName(e.target.value)}
+                    value={lastName}
+                    required="required"
+                    type="text"
+                  />
+                </div>
+                <div className="input-field mb-1">
+                  <input
+                    placeholder="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required="required"
+                    type="text"
+                  />
+                </div>
+                <div className="input-field mb-1">
+                  <select
+                    onChange={(e) => setGender(e.target.value)}
+                    value={gender}
+                    required="required"
+                  >
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                  </select>
+                </div>
+                <div className="input-field mb-2">
+                  <input
+                    placeholder="Password"
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    required="required"
+                    type="password"
+                  />
+                </div>
+                <div className="input-field mb-2">
+                  <input
+                    onChange={(e) => setAvatar(e.target.files[0])}
+                    type="file"
+                  />
+                </div>
+              </form>
+            </Fragment>
+
+            <Fragment key="footer">
+              <button className="btn-success">UPDATE</button>
+            </Fragment>
+          </Modal>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
